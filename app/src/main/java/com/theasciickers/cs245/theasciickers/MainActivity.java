ff2229b6432dfe1fa3c9f0184ea1750a7578ad36
Brian Van Hooser <btvanhooser@cpp.edu>
2016-11-03 17:07:00 -0700

    Initial commit

---
+++ app/src/main/java/com/theasciickers/cs245/theasciickers/MainActivity.java
@@ -0,0 +1,13 @@
+package com.theasciickers.cs245.theasciickers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
