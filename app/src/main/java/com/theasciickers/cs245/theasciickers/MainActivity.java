125c57c1bb5f3a31226a0f0ec1aa9c7b56f4c36b
Brian Van Hooser <btvanhooser@cpp.edu>
2016-11-29 15:41:48 -0800

    Added high score entry

--- app/src/main/java/com/theasciickers/cs245/theasciickers/MainActivity.java
+++ app/src/main/java/com/theasciickers/cs245/theasciickers/MainActivity.java
@@ -14,12 +14,16 @@ import android.support.design.widget.NavigationView;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.res.Configuration;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +33,42 @@ public class MainActivity extends AppCompatActivity {
     private ActionBarDrawerToggle drawerToggle;
     private static AudioPlayer player;
     private boolean musicOn;
+    String INITIALHIGHSCORELIST = "4\n" +
+            "---,0\n" +
+            "---,0\n" +
+            "---,0\n" +
+            "6\n" +
+            "---,0\n" +
+            "---,0\n" +
+            "---,0\n" +
+            "8\n" +
+            "---,0\n" +
+            "---,0\n" +
+            "---,0\n" +
+            "10\n" +
+            "---,0\n" +
+            "---,0\n" +
+            "---,0\n" +
+            "12\n" +
+            "---,0\n" +
+            "---,0\n" +
+            "---,0\n" +
+            "14\n" +
+            "---,0\n" +
+            "---,0\n" +
+            "---,0\n" +
+            "16\n" +
+            "---,0\n" +
+            "---,0\n" +
+            "---,0\n" +
+            "18\n" +
+            "---,0\n" +
+            "---,0\n" +
+            "---,0\n" +
+            "20\n" +
+            "---,0\n" +
+            "---,0\n" +
+            "---,0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +77,21 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
         setContentView(R.layout.activity_main);
         // Initialize application state
-        if (savedInstanceState == null){
+        if (!fileExists(this,"highscore.txt")){
+            try {
+                Log.d("Test","File Created");
+                FileOutputStream fos = openFileOutput("highscore.txt", Context.MODE_PRIVATE);
+                fos.write(INITIALHIGHSCORELIST.getBytes());
+                fos.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            Log.d("Test","File Exists");
+        }
+
+    if (savedInstanceState == null){
             // Start music
             player = new AudioPlayer();
             player.play(this);
@@ -57,6 +111,14 @@ public class MainActivity extends AppCompatActivity {
 
         navDrawer = (NavigationView) findViewById(R.id.nav_view);
         setupDrawerContent(navDrawer);
+    }
+
+    public boolean fileExists(Context context, String filename) {
+        File file = context.getFileStreamPath(filename);
+        if(file == null || !file.exists()) {
+            return false;
+        }
+        return true;
     }
 
     private ActionBarDrawerToggle setupDrawerToggle(){
