1448fa5a02c3156dbf98fc63124cbce1218c918e
Alfredo <ceballosalfredo231@gmail.com>
2016-11-29 14:39:35 -0800

    Added splash screen fragment

---
+++ app/src/main/java/com/theasciickers/cs245/theasciickers/SplashScreenFragment.java
@@ -0,0 +1,45 @@
+package com.theasciickers.cs245.theasciickers;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Alfredo on 11/28/2016.
+ */
+
+public class SplashScreenFragment extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        final Handler handler = new Handler();
+        final Runnable r = new Runnable()
+        {
+            public void run()
+            {
+                setContentView(R.layout.activity_main);
+            }
+        };
+        handler.postDelayed(r, 3000);
+    }
+
+    public void onStop(){
+        super.onStop();
+        this.onDestroy();
+    }
+
+
+}
+
